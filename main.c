21a62a92957f15addb4fddda1565a44e9fd8451f
padawan <padawan@debianUPEM-2016>
2018-01-24 18:11:18 +0100

    indentation

--- main.c
+++ main.c
@@ -18,14 +18,14 @@ int main ()
 	
 
 	//Debut du jeu
-	  MLV_Image *image;
-	  int width = 1080;
-	  int height = 720;
-	  MLV_create_window("fenetre","icone fenetre",width,height);
-	  image = MLV_load_image("pirate.jpg");
-	  MLV_resize_image_with_proportions(image,width,height);
-	  MLV_draw_image(image,0,0);
-	  MLV_actualise_window();
-	   MLV_wait_seconds(10);
+	MLV_Image *image;
+	int width = 1080;
+	int height = 720;
+	MLV_create_window("fenetre","icone fenetre",width,height);
+	image = MLV_load_image("pirate.jpg");
+	MLV_resize_image_with_proportions(image,width,height);
+	MLV_draw_image(image,0,0);
+	MLV_actualise_window();
+	MLV_wait_seconds(10);
  
 }
